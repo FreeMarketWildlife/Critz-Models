1305f08e3af69839935f3d6555bf0da05b7041b4
FreeMarketWildlife <freemarketwildlife@gmail.com>
2025-09-21 21:34:34 -0700

    Add draw speed tooltip and update weapon rarities

--- src/hud/components/WeaponDetailPanel.js
+++ src/hud/components/WeaponDetailPanel.js
@@ -56,6 +56,7 @@ const STAT_TOOLTIP_BUILDERS = {
   ammo: (value) => buildAmmoTooltip(value),
   ammoOverheat: (value) => buildOverheatTooltip(value),
   overheat: (value) => buildOverheatTooltip(value),
+  drawSpeed: () => 'How fast this weapon or tool can be used after switching to it',
 };
 
 const RARITY_TITLES = {
